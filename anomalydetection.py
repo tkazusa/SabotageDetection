e05005a3b896c3f1f27682a749229450cd0f40af
tkazusa <takekazusa@gmail.com>
2018-01-18 10:06:25 +0000

    add data_generator.py

--- anomalydetection.py
+++ anomalydetection.py
@@ -1,34 +1,17 @@
 # coding=utf-8
 
 # write code...
-
-import numpy as np
-
-from sklearn.datasets import make_classification
-from sklearn.cross_validation import train_test_split
-
-#from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-#from sklearn.neighbors import KNeighborsClassifier
-from sklearn import svm
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 
 
+from sklearn.datasets import make_classification
+from sklearn.cross_validation import train_test_split
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
 from sklearn.metrics import roc_auc_score
-
-# サンプルデータの生成
-# 1000 samples、5(infomative) + 2(redundant) + 13(independent) =  20 feature のデータを生成
-#dat = make_classification(n_samples=1000, n_features=106, n_informative=80, n_redundant=20, n_classes=2, weights= [0.99,0.01])
-#X = np.round((dat[0]+30)*2)
-#y = dat[1]
-
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
-
-#clf = RandomForestClassifier(n_estimators=500, random_state=123)
-#clf.fit(X_train, y_train)
-#print("RandomForestClassifier AUC =", roc_auc_score(y_test, clf.predict_proba(X_test)[:,1]))
 
 n_samples = 200
 outliers_fraction = 0.01
@@ -37,9 +20,9 @@ n_outliers = int(outliers_fraction*n_samples)
 
 
 
-X_work = np.round(np.random.normal(500, 200, (n_inliers, 106)))
+X_work = np.round(np.random.normal(500, 200, (n_inliers, 117)))
 y_work  = np.ones(n_inliers)
-X_non_work =  np.round(np.random.normal(5, 1, (n_outliers, 106)))
+X_non_work =  np.round(np.random.normal(5, 1, (n_outliers, 117)))
 y_non_work = np.ones(n_outliers) * -1
 
 X = np.concatenate((X_work, X_non_work), axis=0)
